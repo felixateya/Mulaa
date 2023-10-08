716d5d2a8d597f59ccdfa102849260338d05ee4f
felixateya <mulaafelix6@gmail.com>
2023-10-08 09:42:03 +0300

    new changes

--- src/components/IntroductionText.jsx
+++ src/components/IntroductionText.jsx
@@ -1,7 +1,7 @@
 
 function IntroductionText({className, text}) {
   return (
-    <div className={className} data-aos="fade-left"
+    <div className={className} data-aos="fade-up"
     data-aos-easing="linear"
     data-aos-duration="1200">{text}</div>
   )
