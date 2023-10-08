edded1cbe89c53ee439a8bd2ac4d2de2c32ea535
felixateya <mulaafelix6@gmail.com>
2023-10-08 09:29:13 +0300

    responsiveness

--- src/components/TestimonialCard.jsx
+++ src/components/TestimonialCard.jsx
@@ -2,7 +2,7 @@
   
   function TestimonialCard ({children, className}){
     return (
-      <div className={className} data-aos="fade-left"
+      <div className={className} data-aos="fade-up"
       data-aos-easing="linear"
       data-aos-duration="500">{children}</div>
     )
