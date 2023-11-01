8e0fcc3840e24714581163e82a3b675b7892aed3
felixateya <mulaafelix6@gmail.com>
2023-11-01 20:53:39 +0300

    final touches

--- src/components/IntroductionText.jsx
+++ src/components/IntroductionText.jsx
@@ -1,9 +1,9 @@
 
 function IntroductionText({className, text}) {
   return (
-    <div className={className} data-aos="fade-up"
+    <div className={className} data-aos="fade-out"
     data-aos-easing="linear"
-    data-aos-duration="1200">{text}</div>
+    data-aos-duration="5000">{text}</div>
   )
 }
 
